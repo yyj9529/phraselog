9b851bde0a90620327f1f34ae0863fcc930dd5a2
yyj9529 <67089946+yyj9529@users.noreply.github.com>
2025-10-23 04:05:37 +0900

    Refactor/disable welcome email (#16)
    * refactor(auth): temporaily disable welcome email route
    
    * refactor(cron): remove redundant ts-expect-error comment
    
    * chore: remove temporary log files and unused assets
    
    ---------
    
    Co-authored-by: yyj <garethgates88@gmail.com>

--- app/features/home/screens/welcome.tsx
+++ app/features/home/screens/welcome.tsx
@@ -1,17 +1,17 @@
-import { Resend } from "resend";
-import { WelcomeUser } from "react-email-starter/emails/welcome-user";
-import type { Route } from ".react-router/types/app/features/home/screens/+types/welcome.ts";
+// import { Resend } from "resend";
+// import { WelcomeUser } from "react-email-starter/emails/welcome-user";
+// import type { Route } from ".react-router/types/app/features/home/screens/+types/welcome.ts";
 
 
-const client = new Resend(process.env.RESEND_API_KEY);
+// const client = new Resend(process.env.RESEND_API_KEY);
 
-export const loader = async({params} : Route.LoaderArgs)=>{
-    const {data,error} = await client.emails.send({
-        from : "PhraseLog <wooju@mail.phraselog.online>",
-        to : "garethgates88@gmail.com",
-        subject : "Welcome to Supaplate",
-        react : <WelcomeUser username={params.username} />
-    });
+// export const loader = async({params} : Route.LoaderArgs)=>{
+//     const {data,error} = await client.emails.send({
+//         from : "PhraseLog <wooju@mail.phraselog.online>",
+//         to : "garethgates88@gmail.com",
+//         subject : "Welcome to Supaplate",
+//         react : <WelcomeUser username={params.username} />
+//     });
 
-    return Response.json({data,error});
-}
+//     return Response.json({data,error});
+// }
