d8cdfe94f6cc182eb909297b3700e451d0081376
yyj <garethgates88@gmail.com>
2025-11-25 11:59:13 +0900

    fix: remove static import of instrument.server to bypass build error

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -18,12 +18,11 @@ import type {
   EntryContext,
   HandleErrorFunction,
 } from "react-router";
-import 'instrument.server.mjs';
+
 import { createReadableStreamFromReadable } from "@react-router/node";
 import * as Sentry from "@sentry/node";
 import { createInstance } from "i18next";
 import { isbot } from "isbot";
-import { resolve as resolvePath } from "node:path";
 import { PassThrough } from "node:stream";
 import { renderToPipeableStream } from "react-dom/server";
 import { I18nextProvider, initReactI18next } from "react-i18next";
